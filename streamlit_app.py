0d369f84e910538ece36cb67149243dba6d204b9
JoacoBravo <86753853+JoacoBravo@users.noreply.github.com>
2025-09-16 13:23:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,100 @@
-$ pip install folium
+from folium.features import (
+    Choropleth,
+    ClickForLatLng,
+    ClickForMarker,
+    ColorLine,
+    Control,
+    CustomIcon,
+    DivIcon,
+    GeoJson,
+    GeoJsonPopup,
+    GeoJsonTooltip,
+    LatLngPopup,
+    RegularPolygonMarker,
+    TopoJson,
+    Vega,
+    VegaLite,
+)
+from folium.folium import Map
+from folium.map import (
+    FeatureGroup,
+    FitBounds,
+    FitOverlays,
+    Icon,
+    LayerControl,
+    Marker,
+    Popup,
+    Tooltip,
+)
+from folium.raster_layers import TileLayer, WmsTileLayer
+from folium.utilities import JsCode
+from folium.vector_layers import Circle, CircleMarker, Polygon, PolyLine, Rectangle
+
+try:
+    from ._version import __version__
+except ImportError:  # pragma: no cover
+    __version__ = "unknown"
+
+
+if branca.__version__ != "unknown" and tuple(
+    int(x) for x in branca.__version__.split(".")[:2]
+) < (
+    0,
+    3,
+):  # pragma: no cover
+    raise ImportError(
+        "branca version 0.3.0 or higher is required. "
+        "Update branca with e.g. `pip install branca --upgrade`."
+    )
+
+
+__all__ = [
+    "Choropleth",
+    "ClickForMarker",
+    "ClickForLatLng",
+    "ColorLine",
+    "ColorMap",
+    "Control",
+    "CssLink",
+    "CustomIcon",
+    "Div",
+    "DivIcon",
+    "Element",
+    "FeatureGroup",
+    "Figure",
+    "FitBounds",
+    "FitOverlays",
+    "GeoJson",
+    "GeoJsonPopup",
+    "GeoJsonTooltip",
+    "Html",
+    "IFrame",
+    "Icon",
+    "JavascriptLink",
+    "JsCode",
+    "LatLngPopup",
+    "LayerControl",
+    "LinearColormap",
+    "Link",
+    "MacroElement",
+    "Map",
+    "Marker",
+    "Popup",
+    "RegularPolygonMarker",
+    "StepColormap",
+    "TileLayer",
+    "Tooltip",
+    "TopoJson",
+    "Vega",
+    "VegaLite",
+    "WmsTileLayer",
+    # vector_layers
+    "Circle",
+    "CircleMarker",
+    "PolyLine",
+    "Polygon",
+    "Rectangle",
+]
 import streamlit as st
 import folium
 import pandas as pd
